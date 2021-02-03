8f47bdd9b8a5f6d4a7cf52fd5224eface8341524
Menna Khaliel <56710050+Menna13@users.noreply.github.com>
2021-02-03 15:39:27 -0500

    final work on the app

--- lite/examples/image_classification/android/lib_support/src/main/java/org/tensorflow/lite/examples/classification/tflite/ClassifierQuantizedMobileNet.java
+++ lite/examples/image_classification/android/lib_support/src/main/java/org/tensorflow/lite/examples/classification/tflite/ClassifierQuantizedMobileNet.java
@@ -49,16 +49,14 @@ public class ClassifierQuantizedMobileNet extends Classifier {
 
   @Override
   protected String getModelPath() {
-    // you can download this file from
-    // see build.gradle for where to obtain this file. It should be auto
-    // downloaded into assets.
-    return "mobilenet_v1_1.0_224_quant.tflite";
+    return "model.tflite";
   }
 
   @Override
   protected String getLabelPath() {
     return "labels.txt";
   }
+
 
   @Override
   protected TensorOperator getPreprocessNormalizeOp() {
